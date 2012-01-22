c4dc819c33ae74f5c6668488d14c43528156cd28
Jamie Brandon <jamie@scattered-thoughts.net>
2012-01-22 19:41:26 +0000

    Dont evict from the cache before its full

--- src/cache.py
+++ src/cache.py
@@ -9,6 +9,7 @@ class Random():
         self.max_size = max_size
         self.cache = {}
         self.keys = [None]*max_size
+        self.total = 0
 
     def __contains__(self, key):
         return key in self.cache
@@ -16,15 +17,21 @@ class Random():
     def __getitem__(self, key):
         return self.cache[key]
 
+    def evict(self):
+        index = random.randint(0, self.max_size-1)
+        old_key = self.keys[index]
+        del self.cache[old_key]
+        return index
+
     def __setitem__(self, key, value):
         if key in self.cache:
             self.cache[key] = value
         else:
-            index = random.randint(0, self.max_size-1)
-
-            old_key = self.keys[index]
-            if old_key:
-                del self.cache[old_key]
+            if self.total >= max_size:
+                index = self.evict()
+            else:
+                index = self.total
+                self.total += 1
 
             self.keys[index] = key
             self.cache[key] = value
