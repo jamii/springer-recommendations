dbd79a8221cebf8140537e5688fff446788bbc3d
Jamie Brandon <jamie@scattered-thoughts.net>
2012-03-21 18:27:15 +0000

    Dont drop heavy users

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,3 @@
 root_directory = '/mnt/var/springer-recommendations'
 db_cache_size = 10000
 default_number_of_partitions = 4 # for best performance this should be the same as the total number of disco workers (check the disco web config)
-max_downloads_per_ip = 1000 # ips with more downloads will be ignored, as they cause the majority of the work and probably don't represent an individual person
