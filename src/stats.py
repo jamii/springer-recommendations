25dabc60495b4c82fd6a4cbeaf59b63740eb0ac0
Jamie Brandon <jamie@scattered-thoughts.net>
2012-03-24 15:46:40 +0000

    Update stats

--- src/stats.py
+++ src/stats.py
@@ -1,21 +1,48 @@
 import collections
 import heapq
+import matplotlib.pyplot as plot
 
 import db
 
-def db_value_lengths(db_name, collection_name):
+def value_lengths(iter):
     counter = collections.Counter()
 
-    for key, values in db.DB(db_name, collection_name):
+    for key, values in iter:
         counter[len(values)] += 1
 
-    return [counter[i] for i in xrange(0, max(counter.keys()) + 1)]
+    for i in xrange(0, max(counter.keys()) + 1):
+        yield counter[i]
 
-def db_value_extremes(db_name, collection_name, n):
-    extremes = []
-
+def value_extremes(iter, n, unpack=lambda x: x):
     def lengths():
-        for key, values in db.DB(db_name, collection_name):
+        for key, values in iter:
             yield len(values), key
 
-    return heapq.nlargest(n, lengths())
+    largest = heapq.nlargest(n, lengths())
+
+    return [(unpack(key), value) for value, key in largest]
+
+def plot_doi_si(build_name):
+    plot.subplot(121)
+    doi2sis = db.MultiValue(build_name, 'doi2sis')
+    doi_ids = db.Ids(build_name, 'doi')
+    def unpack(struct):
+        id = db.id_struct.unpack(struct)[0]
+        return doi_ids.get_string(id)
+    print value_extremes(doi2sis, 25, unpack=unpack)
+    plot.loglog(list(value_lengths(doi2sis)))
+    plot.xlabel('Number of sis downloading')
+    plot.ylabel('Frequency')
+
+    plot.subplot(122)
+    si2dois = db.MultiValue(build_name, 'si2dois')
+    si_ids = db.Ids(build_name, 'si')
+    def unpack(struct):
+        id = db.id_struct.unpack(struct)[0]
+        return si_ids.get_string(id)
+    print value_extremes(si2dois, 25, unpack=unpack)
+    plot.loglog(list(value_lengths(si2dois)))
+    plot.xlabel('Number of dois downloaded')
+    plot.ylabel('Frequency')
+
+    plot.show()
